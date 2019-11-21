93ea2afc5d5c03f9e3739ce6ce7c08547bcbb286
Vanessa Lane <vaaklane@gmail.com>
2019-11-21 01:31:50 -0800

    create home app

---
+++ home/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def HomePageView(request):
+    return render(request, 'home.html')
--- vlane_net/settings.py
+++ vlane_net/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['mysterious-stream-39497.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'cookbook.apps.CookbookConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
