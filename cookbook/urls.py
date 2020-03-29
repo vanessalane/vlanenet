b304d5588f9f82d1ee3113a51ea0900ea65f6e33
Vanessa Lane <vaaklane@gmail.com>
2020-03-29 14:01:38 -0700

    Fixes styling and adds a link to create a recipe

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.TableOfContentsView, name='table_of_contents'),
-    path('<recipe_title>', views.RecipeView, name='recipe'),
-    path('add_recipe', views.AddRecipeView, name='add_recipe'),
+    path('recipe/<recipe_title>', views.RecipeView, name='recipe'),
 ]
